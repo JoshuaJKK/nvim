72031afbb82934495b84745fc03a1b551ab8e5b5
JoshuaJKK <163781857+JoshuaJKK@users.noreply.github.com>
2024-07-21 11:04:14 +0100

    Update keymaps.lua

--- lua/config/core/keymaps.lua
+++ lua/config/core/keymaps.lua
@@ -2,5 +2,5 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
-keymap.set("n", "<leader>hl", ":nohl", {})
+keymap.set("n", "<leader>hl", ":nohl<CR>", {})
 keymap.set("n", "<leader>n", ":Neotree<CR>", {})
